abbd027784601d3461b6faef3288731591aa0ff0
yuukimiyo <yuuki.miyo@gmail.com>
2012-11-13 11:23:04 +0900

    alpha

--- js/jquery.resizablecolumns.js
+++ js/jquery.resizablecolumns.js
@@ -38,33 +38,54 @@
 	 */
 	jQuery.fn.resizablecolumns = function(options) {
 		var options = jQuery.extend({
-			resizableColumn : ".resizable",
-			dependentColumn : ".dependent"
+			resizableSelecta : ".resizable",
+			dependentSelecta : ".dependent",
+			offset: 0,
+			maxWidth: "",
+			maxWidthOffset: 0
 		}, options);
 		
 		var target;
+		var wrapperId;
 		
 		return this.each(function() {
 			target = this;
 			
+			resizeDependent(target);
+			setResizableMaxWidth(target);
+			
 			jQuery(window).resize(function() {
 				resizeDependent(target);
 			});
-			
-			jQuery(target).find('#wrapper').resize(function() {
+			jQuery(target).find('#' + jQuery(target).attr("id") ).resize(function() {
 				resizeDependent(target);
 			});
 			
-			jQuery(target).find('.resizable').resize(function() {
+			jQuery(target).find( options.resizableSelecta ).resize(function() {
 				resizeDependent(target);
 			});
 		});
-		function resizeDependent(_target) {
-				jQuery(_target).find('.dependent').width(
+		function setResizableMaxWidth(_target) {
+			var maxWidthValue;
+			if (options.maxWidth == "") {
+				maxWidthValue = (
 					  jQuery(_target).innerWidth()
-					- jQuery(_target).find('.resizable').outerWidth()
-					- (jQuery(_target).find('.dependent').outerWidth() - jQuery(_target).find('.dependent').innerWidth()) 
-				);
+					- ( jQuery(_target).find( options.dependentSelecta ).outerWidth() - jQuery(_target).find( options.dependentSelecta ).innerWidth() )
+					- options.maxWidthOffset
+					) + "px";
+			} else {
+				maxWidthValue = options.maxWidth;
+			}
+			jQuery('#test-area').html((jQuery(_target).find( options.dependentSelecta ).outerWidth() - jQuery(_target).find( options.dependentSelecta ).innerWidth() ));
+			jQuery(_target).find( options.resizableSelecta ).css("max-width",maxWidthValue);
+		}
+		function resizeDependent(_target) {
+			jQuery(_target).find( options.dependentSelecta ).width(
+				  jQuery(_target).innerWidth()
+				- jQuery(_target).find( options.resizableSelecta ).outerWidth()
+				- (jQuery(_target).find ( options.dependentSelecta ).outerWidth() - jQuery(_target).find( options.dependentSelecta ).innerWidth())
+				- options.offset
+			);
 		};
 	};
 })(jQuery);
